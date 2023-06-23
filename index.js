b2d90442ca037409a2550b0837864e063a19fa45
Brian Kibet <kibetsbrian@gmail.com>
2023-06-23 17:34:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
-  
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-  
-    input.addEventListener('click', clickAlert);
-  }
-  
-  addingEventListener();
-  
+  const input = document.getElementById('button');
+  input.addEventListener('click', () => {
+ 
+  });
+}
